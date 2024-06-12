81f2cb799f704b753fc3b159a4d275283101e400
Mohamadou Sy <mohabousy@gmail.com>
2024-06-12 12:00:54 +0200

    Affichage du message de victoire

--- app.js
+++ app.js
@@ -1,155 +1,228 @@
-function setup(){
-    
-    new Canvas(1200,720)
-    world.gravity.y = 10;
-    deadZone = new Sprite()
-    deadZone.width = 1200
-    deadZone.height = 10
-    deadZone.y = 620
-    deadZone.image = 'img/deadZone.png'
-    deadZone.img.scale.x = 1.9
-    deadZone.collider = 'none'
-    //deadZone.debug = 'true'
+let player;
 
+function loadPlayerAnimations() {
+  const idle = loadAni("img/player_idle.png");
+  idle.scale = 0.8;
 
-    Pipe = new Sprite()
-    Pipe.x = 200
-    Pipe.y = 120
-    Pipe.w = 1300
-    Pipe.h = 40
-    Pipe.image = 'img/tuyaux.png'
-    Pipe.img.scale.y = 0.5
-    Pipe.collider = 'static'
-    //Pipe.debug ='true'
+  const walk = loadAni("img/player_walk1.png", "img/player_walk2.png");
+  walk.scale = 0.8;
+  walk.frameDelay = 15;
 
+  const jump = loadAni("img/player_jump.png");
+  jump.scale = 0.8;
 
+  player.addAni("idle", idle);
+  player.addAni("walk", walk);
+  player.addAni("jump", jump);
 
-    PipeTurn = new Sprite()
-    PipeTurn.image = 'img/tile_0093.png'
-    PipeTurn.rotation = 270
-    PipeTurn.x = 879
-    PipeTurn.y = 120
-    PipeTurn.w = 12
-    PipeTurn.h = 13
-    PipeTurn.scale = 3.27
-    PipeTurn.collider = 'static'
-    //PipeTurn.debug ='true'
+  player.changeAni("idle");
+}
+function setup() {
+  new Canvas(1200, 720);
+  world.gravity.y = 10;
+  deadZone = new Sprite();
+  deadZone.width = 1200;
+  deadZone.height = 10;
+  deadZone.y = 620;
+  deadZone.image = "img/deadZone.png";
+  deadZone.img.scale.x = 1.9;
+  deadZone.collider = "none";
+  //deadZone.debug = 'true'
 
+  arrow = new Sprite();
+  arrow.img = "img/signpost.png";
+  arrow.rotation = -90;
+  arrow.scale = 1.5;
+  arrow.y = 30;
+  arrow.x = 200;
+  arrow.collider = "none";
 
-    PipeV = new Sprite()
-    PipeV.x = 879
-    PipeV.y = 280
-    PipeV.h = 40
-    PipeV.w = 1315
-    PipeV.image = 'img/tuyaux.png'
-    PipeV.rotation = 270
-    PipeV.scale.x = 0.2
-    PipeV.collider = 'static'
-    //PipeV.debug ='true'
-    
-    Sol = new Sprite()
-    Sol.x = 600
-    Sol.y = 550
-    Sol.width = 1075
-    Sol.img = 'img/sol.png'
-    Sol.scale = 0.8
-    Sol.collider = 'static'
-    //Sol.debug ='true'
+  pipe = new Sprite();
+  pipe.x = 200;
+  pipe.y = 120;
+  pipe.w = 1300;
+  pipe.h = 40;
+  pipe.image = "img/tuyaux.png";
+  pipe.img.scale.y = 0.5;
+  pipe.collider = "static";
+  //pipe.debug ='true'
 
-    Poutre = new Sprite ()
-    Poutre.x = 440
-    Poutre.y = 275
-    Poutre.w = 110
-    Poutre.h = 345
-    Poutre.img = 'img/poutreRouille.png'
-    Poutre.scale = 0.5
-    Poutre.collider = 'static'
-    //Poutre.debug ='true'
+  pipeTurn = new Sprite();
+  pipeTurn.image = "img/tile_0093.png";
+  pipeTurn.rotation = 270;
+  pipeTurn.x = 879;
+  pipeTurn.y = 120;
+  pipeTurn.w = 12;
+  pipeTurn.h = 13;
+  pipeTurn.scale = 3.27;
+  pipeTurn.collider = "static";
+  //pipeTurn.debug ='true'
 
-    Poutre2 = new Sprite()
-    Poutre2.x = 286
-    Poutre2.y = 427
-    Poutre2.w = 120
-    Poutre2.h = 350
-    Poutre2.img = 'img/poutreRouille.png'
-    Poutre2.scale = 0.5
-    Poutre2.collider = 'static'
-    //Poutre2.debug ='true'
+  pipeV = new Sprite();
+  pipeV.x = 879;
+  pipeV.y = 280;
+  pipeV.h = 40;
+  pipeV.w = 1315;
+  pipeV.image = "img/tuyaux.png";
+  pipeV.rotation = 270;
+  pipeV.scale.x = 0.2;
+  pipeV.collider = "static";
+  //pipeV.debug ='true'
 
-    Poutre3 = new Sprite()
-    Poutre3.x = 30
-    Poutre3.y = 280
-    Poutre3.w = 120
-    Poutre3.h = 360
-    Poutre3.img = 'img/poutreRouille.png'
-    Poutre3.scale = 0.5
-    Poutre3.scale.y = 0.5
-    Poutre3.rotation = 90
-    Poutre3.collider = 'static'
-    //Poutre3.debug ='true'
+  pipeHole = new Sprite();
+  pipeHole.x = 879;
+  pipeHole.y = 435;
+  pipeHole.img = "img/pipeHole.png";
+  pipeHole.mirror.x = "true";
+  pipeHole.img.scale = 3.5;
+  pipeHole.collider = "none";
 
-    Danger = new Sprite()
-    Danger.img = 'img/tile_0042.png'
-    Danger.x = 440
-    Danger.y = 160
-    Danger.w = 15
-    Danger.h = 15
-    Danger.scale = 3
-    Danger.collider = 'static'
-    Danger.debug ='true'
+  Sol = new Sprite();
+  Sol.x = 600;
+  Sol.y = 550;
+  Sol.width = 1075;
+  Sol.img = "img/sol.png";
+  Sol.scale = 0.8;
+  Sol.collider = "static";
+  //Sol.debug ='true'
 
-    Coffre = new Sprite()
-    Coffre.img = 'img/tile_0060.png'
-    Coffre.x = 22
-    Coffre.y = 230
-    Coffre.w = 15
-    Coffre.h = 15
-    Coffre.scale = 2.5
-    Coffre.collider = 'static'
+  beam = new Sprite();
+  beam.x = 440;
+  beam.y = 275;
+  beam.w = 110;
+  beam.h = 345;
+  beam.img = "img/beam.png";
+  beam.scale = 0.5;
+  beam.collider = "static";
+  //beam.debug ='true'
 
-    Player = new Sprite()
+  beam2 = new Sprite();
+  beam2.x = 286;
+  beam2.y = 427;
+  beam2.w = 120;
+  beam2.h = 350;
+  beam2.img = "img/beam.png";
+  beam2.scale = 0.5;
+  beam2.collider = "static";
+  //beam2.debug ='true'
 
-    echelle = new Sprite()
-    echelle.debug = 'true'
-    echelle.x = 1100
-    echelle.y = 360
-    echelle.w = 50
-    echelle.speed = 2
-    echelle.img = 'img/tile_0090.png'
-    echelle.img.scale = 3.5
-    echelle.collider = 'kinematic'
-    
+  beam3 = new Sprite();
+  beam3.x = 30;
+  beam3.y = 280;
+  beam3.w = 120;
+  beam3.h = 360;
+  beam3.img = "img/beam.png";
+  beam3.scale = 0.5;
+  beam3.scale.y = 0.5;
+  beam3.rotation = 90;
+  beam3.collider = "static";
+  //beam3.debug ='true'
 
-    echelle2 = new Sprite()
-    echelle2.debug = 'true'
-    echelle2.x = 1040
-    echelle2.w = 50
-    echelle2.rotation = 180
-    echelle2.img = 'img/tile_0090.png'
-    echelle2.img.scale = 3.5
-    echelle2.collider = 'kinematic'
+  danger = new Sprite();
+  danger.img = "img/tile_0042.png";
+  danger.x = 440;
+  danger.y = 160;
+  danger.w = 15;
+  danger.h = 15;
+  danger.scale = 3;
+  danger.collider = "static";
+  danger.debug = "true";
 
-    
-    
+  coffre = new Sprite();
+  coffre.img = "img/tile_0060.png";
+  coffre.x = 22;
+  coffre.y = 230;
+  coffre.w = 15;
+  coffre.h = 15;
+  coffre.scale = 2.5;
+  coffre.collider = "static";
 
+  player = new Sprite();
+  player.x = 400;
+  player.y = 400;
+  player.w = 50;
+  player.h = 80;
+  player.debug = "true";
+
+  loadPlayerAnimations();
+
+  plateform = new Sprite();
+  plateform.debug = "true";
+  plateform.x = 1100;
+  plateform.y = 360;
+  plateform.w = 50;
+  // plateform.vel.y = -1
+  plateform.speed = 1;
+  plateform.direction = -90;
+
+  plateform.img = "img/tile_0090.png";
+  plateform.img.scale = 3.5;
+  plateform.collider = "kinematic";
+
+  plateform2 = new Sprite();
+  plateform2.debug = "true";
+  plateform2.x = 1040;
+  plateform2.w = 50;
+  plateform2.y = 360;
+  plateform2.speed = 1;
+  plateform2.direction = -90;
+  plateform2.rotation = 180;
+  plateform2.img = "img/tile_0090.png";
+  plateform2.img.scale = 3.5;
+  plateform2.collider = "kinematic";
 }
 
+function draw() {
+  background("white");
+  //deplacer la plateforme de haut en bas : en cours...
+  //   if (plateform.y < 100) {
+  // plateform.direction = 90
+  // }
+  //   if (plateform.y > 300) {
+  // plateform.direction = -90
+  // }
 
-function draw(){ 
-    background('white')
-
-    echelle.y += echelle.speed
-    if (echelle.y > height || echelle.y < 360) {
-    echelle.speed *= -1;
+  if (plateform.y < 150 || plateform.y > 450) {
+    plateform.direction *= -1;
+  }
+  if (plateform2.y < 150 || plateform2.y > 450) {
+    plateform2.direction *= -1;
   }
 
+  /* if (kb.pressing("left")) player.vel.x = -3;
+  else if (kb.pressing("right")) player.vel.x = 3;
+  else player.vel.x = 0;
+  if (kb.pressing("space") && player.vel.y <= 0.2) {
+    player.vel.y = -8;
+  }*/
+  player.rotation = 0;
+  if (kb.pressing("left")) {
+    player.vel.x = -7;
+    player.changeAni("walk");
+    player.mirror.x = true;
+  } else if (kb.pressing("right")) {
+    player.vel.x = 7;
+    player.changeAni("walk");
+    player.mirror.x = false;
+  }
 
+  if (kb.pressing("space")) {
+    player.vel.y = -5;
+    player.changeAni("jump");
+    player.mirror.x = false;
+  }
 
-    if (kb.pressing('left')) Player.vel.x = -3;
-    else if (kb.pressing('right')) Player.vel.x = 3;
-    else Player.vel.x = 0;
-    if (kb.pressing('space') && Player.vel.y <= 0.2) {
-    Player.vel.y = -3; 
+  if (!kb.pressing("left") && !kb.pressing("right") && !kb.pressing("space")) {
+    player.changeAni("idle");
+  }
+  if (kb.pressing("left") && kb.pressing("space")) {
+    player.changeAni("jump");
+    player.mirror.x = true;
+  }
+
+  if (player.x < 0 && player.y < 120) {
+    message = new Sprite();
+    message.textSize = 40;
+    message.text = "Vous avez bien fuit";
+  }
 }
-}
-
